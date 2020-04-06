6020a2ed68cd67e876a59e666fd720f1604525cd
Isaac Bevis <isaacbevis123@gmail.com>
2020-04-06 14:54:56 -0400

    Changing an app to a master/detail layout

--- app/src/main/java/com/example/android/songdetail/MainActivity.java
+++ app/src/main/java/com/example/android/songdetail/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
      */
+    private boolean mTwoPane = false;
 
     /**
      * Sets up a song list as a RecyclerView.
@@ -62,6 +63,10 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.song_list);
         recyclerView.setAdapter
                 (new SimpleItemRecyclerViewAdapter(SongUtils.SONG_ITEMS));
+
+        if (findViewById(R.id.song_detail_container) != null){
+            mTwoPane = true;
+        }
     }
 
     /**
@@ -108,12 +113,21 @@ public class MainActivity extends AppCompatActivity {
             holder.mView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Context context = v.getContext();
-                    Intent intent = new Intent(context,
-                            SongDetailActivity.class);
-                    intent.putExtra(SongUtils.SONG_ID_KEY,
-                            holder.getAdapterPosition());
-                    context.startActivity(intent);
+                    if (mTwoPane) {
+                        int selectedSong = holder.getAdapterPosition();
+                        SongDetailFragment fragment =
+                                SongDetailFragment.newInstance(selectedSong);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.song_detail_container, fragment)
+                                .addToBackStack(null)
+                                .commit();
+                    } else {
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, SongDetailActivity.class);
+                        intent.putExtra(SongUtils.SONG_ID_KEY,
+                                holder.getAdapterPosition());
+                        context.startActivity(intent);
+                    }
                 }
             });
         }
